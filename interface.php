4b3d7695b41c22889aa53b516bd1c585506d0876
hilmialmuhtadeb <hilmialmuhtadeb@gmail.com>
2021-02-12 11:33:44 +0700

    belajar materi interface-implements

---
+++ interface.php
@@ -0,0 +1,153 @@
+<?php
+
+// interface
+interface InfoProduk {
+  public function getInfoProduk();
+}
+
+// class parent
+
+abstract class Produk {
+  protected $judul,
+    $penulis,
+    $penerbit,
+    $harga,
+    $diskon = 0;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  public function getLabel() {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  // abstract method 
+
+  abstract public function getInfo();
+
+  // setter dan getter
+
+  public function setJudul($judul) {
+    return $this->judul = $judul;
+  }
+
+  public function getJudul() {
+    return $this->judul;
+  }
+
+  public function setPenulis($penulis) {
+    return $this->penulis = $penulis;
+  }
+
+  public function getPenulis() {
+    return $this->penulis;
+  }
+
+  public function setPenerbit($penerbit) {
+    return $this->penerbit = $penerbit;
+  }
+
+  public function getPenerbit() {
+    return $this->penerbit;
+  }
+
+  public function setDiskon($diskon) {
+    return $this->diskon = $diskon;
+  }
+
+  public function getDiskon() {
+    return $this->diskon;
+  }
+
+  public function setHarga($harga) {
+    return $this->harga = $harga;
+  }
+
+  public function getHarga() {
+    return $this->harga - ($this->harga * $this->diskon / 100);
+  }
+}
+
+// =================================================
+// class komik
+
+class Komik extends Produk implements InfoProduk {
+  public $jmlHalaman;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0) {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+
+    $this->jmlHalaman = $jmlHalaman;
+  }
+
+  public function getInfo() {
+    $str = "{$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
+
+    return $str;
+  }
+
+  public function getInfoProduk() {
+    $str = "Komik :   " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
+
+    return $str;
+  }
+}
+
+// =================================================
+// class game
+
+class Game extends produk implements InfoProduk {
+  public $waktuMain;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0) {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+
+    $this->waktuMain = $waktuMain;
+  }
+
+  public function getInfo() {
+    $str = "{$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
+
+    return $str;
+  }
+
+  public function getInfoProduk() {
+    $str = "Game : " . $this->getInfo() . " ~ {$this->waktuMain} Jam.";
+
+    return $str;
+  }
+}
+
+// class cetak info
+
+class CetakInfoProduk {
+  public $daftarProduk = array();
+
+  public function tambahProduk(Produk $produk) {
+    $this->daftarProduk[] = $produk;
+  }
+
+  public function cetak() {
+    $str = "DAFTAR PRODUK : <br>";
+
+    foreach ($this->daftarProduk as $p) {
+      $str .= "- {$p->getInfoProduk()} <br>";
+    }
+
+    return $str;
+  }
+}
+
+// code program
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("GTA V", "David Jones", "Rockstar Games", 500000, 600);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
